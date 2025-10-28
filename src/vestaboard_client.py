ec696178f11d69f41cb4812202ae3a8d9ce94f44
James Fryman <james@fryman.io>
2025-10-27 22:21:57 -0500

    Update mapping

--- src/vestaboard_client.py
+++ src/vestaboard_client.py
@@ -19,8 +19,13 @@ CHAR_CODE_MAP = {
     **{i: chr(ord('A') + i - 1) for i in range(1, 27)},  # A-Z (1-26)
     27: '1', 28: '2', 29: '3', 30: '4', 31: '5', 32: '6',  # 1-9 (27-35)
     33: '7', 34: '8', 35: '9', 36: '0',                    # 0 is 36
-    37: '!', 38: '@', 39: '#', 40: '$', 41: '(', 42: ')',
-    44: '-', 46: '.', 47: '/', 59: ':', 63: '?', 64: '°'
+    37: '!', 38: '@', 39: '#', 40: '$', 41: '(', 42: ')',  # punctuation
+    44: '-', 46: '+', 47: '&', 48: '=', 49: ';', 50: ':',  # punctuation continued
+    52: "'", 53: '"', 54: '%', 55: ',', 56: '.', 59: '/',  # punctuation continued
+    60: '?', 62: '♥',  # question mark, heart (Note) / degree (Flagship)
+    # Color codes
+    63: '🟥', 64: '🟧', 65: '🟨', 66: '🟩', 67: '🟦', 68: '🟪',  # red, orange, yellow, green, blue, violet
+    69: '⬜', 70: '⬛', 71: '⬛'  # white, black, filled
 }
 
 # Reverse mapping for text-to-code conversion
@@ -29,8 +34,13 @@ TEXT_TO_CODE_MAP = {
     **{chr(ord('A') + i): i + 1 for i in range(26)},  # A-Z -> 1-26
     '1': 27, '2': 28, '3': 29, '4': 30, '5': 31,       # 1-9 -> 27-35
     '6': 32, '7': 33, '8': 34, '9': 35, '0': 36,       # 0 -> 36
-    '!': 37, '@': 38, '#': 39, '$': 40, '(': 41, ')': 42,
-    '-': 44, '.': 46, '/': 47, ':': 59, '?': 63, '°': 64
+    '!': 37, '@': 38, '#': 39, '$': 40, '(': 41, ')': 42,  # punctuation
+    '-': 44, '+': 46, '&': 47, '=': 48, ';': 49, ':': 50,  # punctuation continued
+    "'": 52, '"': 53, '%': 54, ',': 55, '.': 56, '/': 59,  # punctuation continued
+    '?': 60, '♥': 62, '❤': 62,  # question mark, heart (both ♥ and ❤ map to same code)
+    # Color codes (emoji to code mapping)
+    '🟥': 63, '🟧': 64, '🟨': 65, '🟩': 66, '🟦': 67, '🟪': 68,  # red, orange, yellow, green, blue, violet
+    '⬜': 69, '⬛': 70, '■': 71  # white, black, filled
 }
 
 # Board dimensions for different Vestaboard models
